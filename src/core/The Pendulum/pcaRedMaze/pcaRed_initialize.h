3fb018ff89910af5ad360c4a77177d1b0e81a936
brianadizio <33075782+brianadizio@users.noreply.github.com>
2025-02-07 15:37:46 -0500

    Reorganized solution structure

---
+++ src/core/The Pendulum/pcaRedMaze/pcaRed_initialize.h
@@ -0,0 +1,28 @@
+/*
+ * pcaRed_initialize.h
+ *
+ * Code generation for function 'pcaRed_initialize'
+ *
+ */
+
+#ifndef PCARED_INITIALIZE_H
+#define PCARED_INITIALIZE_H
+
+/* Include files */
+#include "rtwtypes.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Function Declarations */
+extern void pcaRed_initialize(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+/* End of code generation (pcaRed_initialize.h) */
